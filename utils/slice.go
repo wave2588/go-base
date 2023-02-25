647fc01c5e99ce4a530b4b91c902d2c8894c73a7
wave <15711367321@163.com>
2023-02-25 17:34:37 +0800

    fix

---
+++ utils/slice.go
@@ -0,0 +1,32 @@
+package utils
+
+func RemoveDuplicates(dedup []int64) []int64 {
+	if len(dedup) <= 1 {
+		return dedup
+	}
+
+	for i := 0; i < len(dedup)-1; i++ {
+		for j := i + 1; j < len(dedup); j++ {
+			if dedup[i] != dedup[j] {
+				continue
+			}
+
+			if j != len(dedup)-1 {
+				dedup[j] = dedup[len(dedup)-1]
+				j--
+			}
+			dedup = dedup[:len(dedup)-1]
+		}
+	}
+
+	return dedup
+}
+
+func IsContainStrings(items []string, item string) bool {
+	for _, eachItem := range items {
+		if eachItem == item {
+			return true
+		}
+	}
+	return false
+}
